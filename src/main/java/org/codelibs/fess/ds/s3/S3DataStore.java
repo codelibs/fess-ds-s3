9b42ed0d14ccaa4b6202475aeb4ab16cd4e1c747
YutaMatsushima <y-mats.en-312@moegi.waseda.jp>
2019-06-06 14:27:07 +0900

    sample -> s3

--- src/main/java/org/codelibs/fess/ds/s3/S3DataStore.java
+++ src/main/java/org/codelibs/fess/ds/s3/S3DataStore.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2018 CodeLibs Project and the Others.
+ * Copyright 2012-2019 CodeLibs Project and the Others.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-package org.codelibs.fess.ds.sample;
+package org.codelibs.fess.ds.s3;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -31,11 +31,11 @@ import org.codelibs.fess.util.ComponentUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class SampleDataStore extends AbstractDataStore {
-    private static final Logger logger = LoggerFactory.getLogger(SampleDataStore.class);
+public class S3DataStore extends AbstractDataStore {
+    private static final Logger logger = LoggerFactory.getLogger(S3DataStore.class);
 
     protected String getName() {
-        return "Sample";
+        return "S3";
     }
 
     @Override
