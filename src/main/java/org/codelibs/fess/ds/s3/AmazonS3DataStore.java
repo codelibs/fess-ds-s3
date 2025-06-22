70a77955e0d131cf37f6a9300039f0b5bf5f6044
Shinsuke Sugaya <shinsuke@apache.org>
2025-06-22 18:31:35 +0900

    Update parent POM version to 15.0.0

--- src/main/java/org/codelibs/fess/ds/s3/AmazonS3DataStore.java
+++ src/main/java/org/codelibs/fess/ds/s3/AmazonS3DataStore.java
@@ -54,11 +54,11 @@ import org.codelibs.fess.crawler.helper.MimeTypeHelper;
 import org.codelibs.fess.ds.AbstractDataStore;
 import org.codelibs.fess.ds.callback.IndexUpdateCallback;
 import org.codelibs.fess.entity.DataStoreParams;
-import org.codelibs.fess.es.config.exentity.DataConfig;
 import org.codelibs.fess.exception.DataStoreCrawlingException;
 import org.codelibs.fess.helper.CrawlerStatsHelper;
 import org.codelibs.fess.helper.CrawlerStatsHelper.StatsAction;
 import org.codelibs.fess.helper.CrawlerStatsHelper.StatsKeyObject;
+import org.codelibs.fess.opensearch.config.exentity.DataConfig;
 import org.codelibs.fess.util.ComponentUtil;
 import org.lastaflute.di.core.exception.ComponentNotFoundException;
 
